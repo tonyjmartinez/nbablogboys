21ce725f8bfab58588ce2fdbc806df8cdb5edeb9
Tony Martinez <ajosephmartinez@gmail.com>
2018-12-28 23:33:09 -0800

    Initial Commit

---
+++ src/ui/menu/SliderMenu.js
@@ -0,0 +1,57 @@
+import React, { Component } from "react";
+
+import { slide as Menu } from "react-burger-menu";
+
+var styles = {
+  bmBurgerButton: {
+    position: "fixed",
+    width: "36px",
+    height: "30px",
+    left: "36px",
+    top: "36px"
+  },
+  bmBurgerBars: {
+    background: "#373a47"
+  },
+  bmCrossButton: {
+    height: "24px",
+    width: "24px"
+  },
+  bmCross: {
+    background: "#bdc3c7"
+  },
+  bmMenu: {
+    background: "#373a47",
+    padding: "2.5em 1.5em 0",
+    fontSize: "1.15em"
+  },
+  bmMorphShape: {
+    fill: "#373a47"
+  },
+  bmItemList: {
+    color: "#b8b7ad",
+    padding: "0.8em"
+  },
+  bmItem: {
+    display: "inline-block"
+  },
+  bmOverlay: {
+    background: "rgba(0, 0, 0, 0.3)"
+  }
+};
+
+class SliderMenu extends React.Component {
+  showSettings(event) {
+    event.preventDefault();
+  }
+
+  render() {
+    return (
+      <Menu styles={styles}>
+        <a style={styles.bmItem}>Home</a>
+      </Menu>
+    );
+  }
+}
+
+export default SliderMenu;
